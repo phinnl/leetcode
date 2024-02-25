edf92f3b3ba2bf5124001e5b140117b1e71abb15
Phin-Nguyen Long Phin <nguyen.long.phin@vsi-international.com>
2024-02-25 20:08:42 +0700

    first commit

---
+++ src/add_two_numbers.rs
@@ -0,0 +1,113 @@
+pub mod add_two_sum {
+    #[derive(PartialEq, Eq, Clone, Debug)]
+    pub struct ListNode {
+        pub val: i32,
+        pub next: Option<Box<ListNode>>,
+    }
+
+    pub fn add_two_numbers(
+        l1: Option<Box<ListNode>>,
+        l2: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
+        if l1 == None {
+            return l2;
+        }
+        if l2 == None {
+            return l1;
+        }
+        let mut head = Some(Box::new(ListNode { val: 0, next: None }));
+        let mut l1_clone: Option<Box<ListNode>> = l1.clone();
+        let mut l2_clone: Option<Box<ListNode>> = l2.clone();
+
+        let mut tail_head = head.as_mut();
+        let mut tail_1 = l1_clone.as_mut();
+        let mut tail_2 = l2_clone.as_mut();
+        let mut is_increase = false;
+        loop {
+            let tail_1_some = tail_1.is_some();
+            let tail_2_some = tail_2.is_some();
+            if !tail_1_some && !tail_2_some {
+                break;
+            }
+            let node_1_num = if tail_1_some {
+                let node_1 = tail_1.unwrap();
+                tail_1 = node_1.next.as_mut();
+                node_1.val
+            } else {
+                tail_1 = None;
+                0
+            };
+            let node_2_num = if tail_2_some {
+                let node_2 = tail_2.unwrap();
+                tail_2 = node_2.next.as_mut();
+                node_2.val
+            } else {
+                tail_2 = None;
+                0
+            };
+            let mut sum = if is_increase {
+                node_1_num + node_2_num + 1
+            } else {
+                node_1_num + node_2_num
+            };
+            let node_head: &mut Box<ListNode> = tail_head.unwrap();
+            if sum >= 10 {
+                is_increase = true;
+                sum = sum - 10;
+            } else {
+                is_increase = false;
+            }
+            if tail_2.is_some() || tail_1.is_some() || is_increase {
+                node_head.next = Some(Box::new(ListNode {
+                    val: if is_increase { 1 } else { 0 },
+                    next: None,
+                }));
+            } else {
+                node_head.next = None;
+            }
+            node_head.val = sum;
+            tail_head = node_head.next.as_mut();
+        }
+        return head;
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn example_1() {
+            assert_eq!(
+                add_two_numbers(
+                    Some(Box::new(ListNode {
+                        val: 2,
+                        next: Some(Box::new(ListNode {
+                            val: 4,
+                            next: Some(Box::new(ListNode { val: 3, next: None }))
+                        }))
+                    })),
+                    Some(Box::new(ListNode {
+                        val: 5,
+                        next: Some(Box::new(ListNode {
+                            val: 6,
+                            next: Some(Box::new(ListNode {
+                                val: 4,
+                                next: Some(Box::new(ListNode { val: 3, next: None }))
+                            }))
+                        }))
+                    }))
+                ),
+                Some(Box::new(ListNode {
+                    val: 7,
+                    next: Some(Box::new(ListNode {
+                        val: 0,
+                        next: Some(Box::new(ListNode {
+                            val: 8,
+                            next: Some(Box::new(ListNode { val: 3, next: None }))
+                        }))
+                    }))
+                }))
+            )
+        }
+    }
+}
---
+++ src/add_two_sum.rs
@@ -0,0 +1,68 @@
+pub mod add_two_sum {
+    use std::collections::HashMap;
+
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let map: HashMap<i32, i32> = nums
+            .iter()
+            .enumerate()
+            .map(|(index, &item)| (item, i32::try_from(index).unwrap()))
+            .collect();
+        println!("{:?}", map);
+        let mut vec: Option<Vec<i32>> = None;
+        for (index, &item) in nums[..(nums.len() - 1)].iter().enumerate() {
+            let index_i32 = i32::try_from(index).unwrap();
+            if let Some(&minus_index) = map.get(&(target - item)) {
+                if minus_index == index_i32 {
+                    continue;
+                }
+                vec = Some(vec![index_i32, minus_index]);
+                break;
+            }
+        }
+        vec.unwrap()
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn example_1() {
+            let nums = vec![2, 7, 11, 15];
+            let target = 9;
+            let output = two_sum(nums.clone(), target);
+            let expect = vec![0, 1];
+            assert_eq!(
+                output, expect,
+                "nums: {:?}, target: {}, expect: {:?}, output: {:?}",
+                &nums, target, &expect, &output
+            );
+        }
+
+        #[test]
+        fn example_2() {
+            let nums = vec![3,2,4];
+            let target = 6;
+            let output = two_sum(nums.clone(), target);
+            let expect = vec![1, 2];
+            assert_eq!(
+                output, expect,
+                "nums: {:?}, target: {}, expect: {:?}, output: {:?}",
+                &nums, target, &expect, &output
+            );
+        }
+
+        #[test]
+        fn example_3() {
+            let nums = vec![3, 3];
+            let target = 6;
+            let output = two_sum(nums.clone(), target);
+            let expect = vec![0, 1];
+            assert_eq!(
+                output, expect,
+                "nums: {:?}, target: {}, expect: {:?}, output: {:?}",
+                &nums, target, &expect, &output
+            );
+        }
+    }
+}
